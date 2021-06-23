37802b5a70e3e1afa0699de6a853da3be73a95e8
bonlover <yungyalsen@gmail.com>
2021-06-22 20:56:02 -0400

    Initialized the project and first commit

---
+++ managementtool/src/main/java/io/wowtech/managementtool/ManagementtoolApplication.java
@@ -0,0 +1,13 @@
+package io.wowtech.managementtool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagementtoolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ManagementtoolApplication.class, args);
+    }
+
+}
---
+++ managementtool/src/test/java/io/wowtech/managementtool/ManagementtoolApplicationTests.java
@@ -0,0 +1,13 @@
+package io.wowtech.managementtool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagementtoolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
