421a81223d447418abf194ddb695882ed7ed42dc
bonlover <yungyalsen@gmail.com>
2021-06-23 11:32:03 -0400

    Edited invalid i/o error handling process

--- managementtool/src/main/java/io/wowtech/managementtool/controllers/ProjectController.java
+++ managementtool/src/main/java/io/wowtech/managementtool/controllers/ProjectController.java
@@ -7,9 +7,12 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.*;
+import java.util.HashMap;
 
 @RestController
 @RequestMapping("/api/project")
@@ -20,9 +23,15 @@ public class ProjectController {
 
     @PostMapping("")
     public ResponseEntity<?> createNewProject(@Valid @RequestBody  Project project, BindingResult result){
+
         if (result.hasErrors()){
-            return  new ResponseEntity<String>("Invalid project Object", HttpStatus.BAD_REQUEST);
+            Map<String, String> errorMap = new HashMap<>();
+            for(FieldError error: result.getFieldErrors()){
+                errorMap.put(error.getField(), error.getDefaultMessage());
+            }
+            return  new ResponseEntity<Map<String, String>>(errorMap, HttpStatus.BAD_REQUEST);
         }
+
         Project project1 = projectService.saveOrUpdateProject(project);
         return new ResponseEntity<Project>(project, HttpStatus.CREATED);
     }
